ea2407ff5c115d9eb359212054477d230ecb08aa
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-08 21:00:33 -0400

    Will this commit work?

---
+++ Leetcode Problems and Solutions/11_Container_With_Most_Water.py
@@ -0,0 +1,10 @@
+# Link to Problem: https://leetcode.com/problems/container-with-most-water/
+# Difficulty - Medium
+
+# Leetcode Problem number 5: Container with most water - Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+from typing import List, Tuple, Dict
+
+def maxArea(height: List[int]) -> int:
+    pass
+
