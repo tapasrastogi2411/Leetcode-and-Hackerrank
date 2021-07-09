ca552e9f20aebf2fce163cf56bbbccca15b8751b
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-08 20:40:24 -0400

    Added starter code for longest_palindromic string problem :(

---
+++ Leetcode Problems and Solutions/5_Longest_Palindromic_String.py
@@ -0,0 +1,13 @@
+# Link to Problem: https://leetcode.com/problems/longest-palindromic-substring/
+# Difficulty - Medium
+
+# Leetcode Problem number 5: Two Sum - Given a string s, return the longest palindromic substring in s.
+
+def longestPalindrome(s: str) -> str:
+    pass
+
+
+# Test-cases to test functionality
+
+input_string = "cbbd"
+print(longestPalindrome(input_string))
