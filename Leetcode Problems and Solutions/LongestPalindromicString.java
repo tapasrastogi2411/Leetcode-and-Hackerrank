31b1c566f327c81bf93ad0fc22797a84c54237ec
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-09 23:32:56 -0400

    Added starter code for Longest Palindromic String in Java

---
+++ Leetcode Problems and Solutions/LongestPalindromicString.java
@@ -0,0 +1,25 @@
+/**
+ * LongestPalindromicString
+ */
+public class LongestPalindromicString {
+
+    public char firstNonRepeatingCharacter(String s){
+        
+        int [] myArray = {10, 20, 30};
+        for(int i = 0; i < myArray.length; i++){
+
+            System.out.println(myArray[i]);
+        }
+
+        return 'c';
+
+    }
+
+    public static void main(String[] args) {
+        
+        LongestPalindromicString instance = new LongestPalindromicString();
+        instance.firstNonRepeatingCharacter("Hello World");
+    }
+
+    
+}
