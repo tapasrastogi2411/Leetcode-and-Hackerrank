8e6bdd0b8405bf827a9498f89fc0133ae110d7f5
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-12 23:16:11 -0400

    Added starter code for the 3sum problem in python

---
+++ Leetcode Problems and Solutions/3sum/15_3sum.py
@@ -0,0 +1,14 @@
+# Link to Problem: https://leetcode.com/problems/two-sum/
+# Difficulty - Medium
+
+# Leetcode Problem number 1: 3Sum - Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+from typing import List, Tuple, Dict
+
+def threeSum(nums: List[int]) -> List[List[int]]:
+    pass
+
+# Testing
+input_list = [-1,0,1,2,-1,-4]
+
+print(threeSum(input_list))
