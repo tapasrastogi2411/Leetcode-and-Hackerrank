30e66ad5ac4cbd36151a757bf071b9e44742c827
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-14 14:01:25 -0400

    Added python starter code for first hard difficulty problem

---
+++ Leetcode Problems and Solutions/Hard_Difficulty/4_Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,18 @@
+# Link to Problem: https://leetcode.com/problems/two-sum/
+# Difficulty - Medium
+
+# Leetcode Problem number 4: Median of Two Sorted Arrays - Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+# The overall run time complexity should be O(log (m+n)).
+
+from typing import List, Tuple, Dict
+
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    pass
+
+# Testing area
+
+input_list1 = [1,3]
+input_list2 = [2]
+
+print(findMedianSortedArrays(input_list1, input_list2))
