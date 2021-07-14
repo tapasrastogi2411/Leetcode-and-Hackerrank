b074a8f7edbb0c0870f380e41eff3c2528a9ee61
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-14 00:02:55 -0400

    Added python starter code for the Minimum Path Sum Problem

---
+++ Leetcode Problems and Solutions/Minimum_Path_Sum/64_Miniimum_Path_Sum.py
@@ -0,0 +1,16 @@
+# Link to Problem: https://leetcode.com/problems/number-of-islands/
+# Difficulty - Medium
+
+# Leetcode Problem number 64: Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+from typing import List, Tuple, Dict
+def minPathSum(grid: List[List[int]]) -> int:
+    pass
+
+# Testing
+grid = [
+        [1,3,1],
+        [1,5,1],
+        [4,2,1]
+        ]
+print(minPathSum(grid))
