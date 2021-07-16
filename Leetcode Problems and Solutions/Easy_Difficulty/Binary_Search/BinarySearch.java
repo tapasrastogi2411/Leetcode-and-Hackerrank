82a392c0bbafc9ea03b5c9f883ae8fc37773bb37
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-15 23:38:04 -0400

    Added starter code in java for the binary search problem

---
+++ Leetcode Problems and Solutions/Easy_Difficulty/Binary_Search/BinarySearch.java
@@ -0,0 +1,28 @@
+/* Link to problem: https://leetcode.com/problems/binary-search/
+Difficulty: Easy
+
+Leetcode Problem no 704: Binary Search - Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+package Easy_Difficulty.Binary_Search;
+
+public class BinarySearch {
+    
+    public static boolean performBinarySearch(int [] array, int targetVariable){
+
+        System.out.println("Go to main function");
+        return true;
+
+    }
+
+    
+
+public static void main(String[] args) {
+
+    System.out.println("There will be a day");
+    
+}
+
+}
