adc2c17a7eef6f3495373ad41f407fc57c1edbf3
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-19 23:57:52 -0400

    Adde python starter code for Binary search problem

---
+++ Leetcode Problems and Solutions/Easy_Difficulty/Binary_Search/704_BinarySearch.py
@@ -0,0 +1,19 @@
+# Link to Problem: https://leetcode.com/problems/two-sum/
+# Difficulty - Medium
+
+# Leetcode Problem number 704: Binary Search - Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+ # You must write an algorithm with O(log n) runtime complexity.
+
+
+from typing import List, Tuple, Dict
+
+def search(nums1: List[int], target: int) -> int:
+    pass
+
+# Testing area
+
+input_list =  [-1,0,3,5,9,12]
+input_target = 9
+
+print(search(input_list1, input_target))
