c75641de740e37f8084a2f1f9ea4f94292fd8c3a
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-21 22:08:54 -0400

    Added accepted python code for Binary Search

--- Leetcode Problems and Solutions/Easy_Difficulty/Binary_Search/704_BinarySearch.py
+++ Leetcode Problems and Solutions/Easy_Difficulty/Binary_Search/704_BinarySearch.py
@@ -8,12 +8,34 @@
 
 from typing import List, Tuple, Dict
 
-def search(nums1: List[int], target: int) -> int:
-    pass
+def search(nums: List[int], target: int) -> int:
+    
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+
+	    #Calculate middle number (mid)
+        mid = (left+right) // 2
+        print(f"nums[mid] = {mid}")
+		#If mid is equal to target
+        if nums[mid] == target:
+             return mid
+
+	    #If mid is greater than target then right is equal to the index before mid
+        if nums[mid] > target:
+             right = mid - 1
+
+	    #If mid is less than target then left is equal to the index after mid
+        else:
+             left = mid + 1
+
+	#If target is not found 
+    return -1
 
 # Testing area
 
 input_list =  [-1,0,3,5,9,12]
 input_target = 9
 
-print(search(input_list1, input_target))
+print(search(input_list, input_target))
