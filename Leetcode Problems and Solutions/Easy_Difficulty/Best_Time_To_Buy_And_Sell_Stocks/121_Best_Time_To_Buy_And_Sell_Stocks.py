c2e5bf61c113c8c433e455b88333568f7c513a2e
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-24 22:36:02 -0400

    Added basic python code which works on smaller test cases

--- Leetcode Problems and Solutions/Easy_Difficulty/Best_Time_To_Buy_And_Sell_Stocks/121_Best_Time_To_Buy_And_Sell_Stocks.py
+++ Leetcode Problems and Solutions/Easy_Difficulty/Best_Time_To_Buy_And_Sell_Stocks/121_Best_Time_To_Buy_And_Sell_Stocks.py
@@ -9,11 +9,34 @@
 
 from typing import List, Tuple, Dict
 
+# importing the module
+import timeit
+  
+
 def maxProfit(prices: List[int]) -> int:
+    
+    # An array to store the profit to get the maximum profit from
+    profit_array = []
+    # Start the first iteration of the list
+    for buy_price_index in range(len(prices)):
+        buy_price_to_consider = prices[buy_price_index]
+        for sell_price_index in range(buy_price_index + 1, len(prices)):
+            # Use prices[buy_price_index] and prices[sell_price_index] to get profit in each case
+
+            profit = prices[sell_price_index] - prices[buy_price_index]
+            profit_array.append(profit)
+        
+    if max(profit_array) <= 0:
+        return 0
+    
+    return max(profit_array)
+                
+            
+
     pass
 
 # Testing
 
-input_prices = [7, 1, 5, 3, 6, 4]
+input_prices = [10000,9999,9998,9997,9996,9995,9994,9993,9992,9991,9990,9989,9988,9987,9986,9985,9984,9983,9982,9981,9980]
 
 print(maxProfit(input_prices))
