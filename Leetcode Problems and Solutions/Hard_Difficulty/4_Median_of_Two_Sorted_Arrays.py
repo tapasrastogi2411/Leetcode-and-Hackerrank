a949b3fe2a7e289b0480d519bdb5d951716d3810
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-25 22:46:46 -0400

    Added accepted Python solution to Median of two sorted arrays

--- Leetcode Problems and Solutions/Hard_Difficulty/4_Median_of_Two_Sorted_Arrays.py
+++ Leetcode Problems and Solutions/Hard_Difficulty/4_Median_of_Two_Sorted_Arrays.py
@@ -6,13 +6,55 @@
 # The overall run time complexity should be O(log (m+n)).
 
 from typing import List, Tuple, Dict
+import math
+
+def merge_two_sorted_lists(nums1: List[int], nums2: List[int]) -> List[int]:
+    
+    if(len(nums1) < len(nums2)):
+        # We have to append nums1 elements to nums2
+        for each_num in nums1:
+            nums2.append(each_num)
+        
+        return nums2
+    
+    else:
+        for each_num in nums2:
+            nums1.append(each_num)
+        
+        return nums1
+    
+    
 
 def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
-    pass
+
+    
+    # Part 1 to consider: How to merge two uneven sorted lists
+    # Try doing that through a helper function - merge_two_sorted_lists
+
+    merged_list = merge_two_sorted_lists(nums1, nums2)
+    sorted_merged_list = sorted(merged_list)
+    
+    if(len(sorted_merged_list)% 2 == 0):
+        # We have an even numbered list, and median == (middle + (middle + 1) / 2) where middle == (len(list) // 2)  and (len(list) // 2) - 1
+        index1 = len(sorted_merged_list) // 2
+        index2 = index1 - 1
+        
+        number1 = sorted_merged_list[index1]
+        number2 = sorted_merged_list[index2]
+
+        median = (number1 + number2) / 2
+        
+    else:
+        middleIndex = math.floor(len(sorted_merged_list) / 2)
+        median = float(sorted_merged_list[middleIndex])
+    
+    return median
 
 # Testing area
 
-input_list1 = [1,3]
+input_list1 = [1, 3]
 input_list2 = [2]
+
+# print(merge_two_sorted_lists(input_list1, input_list2))
 
 print(findMedianSortedArrays(input_list1, input_list2))
