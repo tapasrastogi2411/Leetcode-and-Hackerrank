5468b228d3bae5982ca083713b8e5f7ce65f57ef
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-25 21:31:05 -0400

    Added accepted C solution for Best Time problem

--- Leetcode Problems and Solutions/Easy_Difficulty/Best_Time_To_Buy_And_Sell_Stocks/121_Best_Time_To_Buy_And_Sell_Stocks.c
+++ Leetcode Problems and Solutions/Easy_Difficulty/Best_Time_To_Buy_And_Sell_Stocks/121_Best_Time_To_Buy_And_Sell_Stocks.c
@@ -22,11 +22,36 @@ int maxProfit(int* prices, int pricesSize){
     
 }
 
+int maxProfitVersion2(int *prices, int pricesSize){
+
+    int max_profit = 0;
+    int buy_price_to_consider = prices[0];
+
+    if (pricesSize < 2){
+        return 0;
+    }
+    for (int i = 1; i < pricesSize; i++){
+        if(prices[i] < buy_price_to_consider){
+            buy_price_to_consider = prices[i];
+        }
+        else if(prices[i] > buy_price_to_consider){
+            int profit = prices[i] - buy_price_to_consider;
+            if(profit > max_profit){
+                max_profit = profit;
+            }
+        }
+        
+
+        
+    }
+    return max_profit;
+}
+
 int main(){
 
-    int input_list[] = {7,6,4,3,1};
+    int input_list[] = {7, 1, 5, 3, 6, 4};
     
-    printf("%d\n", maxProfit(input_list, 5));
+    printf("%d\n", maxProfitVersion2(input_list, 6));
 
     return 0;
 
