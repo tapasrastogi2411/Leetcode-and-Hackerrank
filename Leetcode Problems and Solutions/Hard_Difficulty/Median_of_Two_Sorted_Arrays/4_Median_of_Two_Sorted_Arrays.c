6a56392cbab535701c764382480b7cc66c72a272
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-25 23:50:50 -0400

    Added starter C code for Median of Two sorted array problem

---
+++ Leetcode Problems and Solutions/Hard_Difficulty/Median_of_Two_Sorted_Arrays/4_Median_of_Two_Sorted_Arrays.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
+    
+    
+    return 2.0;
+}
+
+
+int main(){
+
+    int input_list1[] = {1, 3};
+    int input_list2[] = {2};
+
+    printf("%f\n", findMedianSortedArrays(input_list1, 2, input_list2, 1));
+
+    return 0;
+}
