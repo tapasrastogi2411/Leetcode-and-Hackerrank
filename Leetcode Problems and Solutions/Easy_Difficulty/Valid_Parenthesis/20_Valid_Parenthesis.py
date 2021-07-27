eab859604c200b0c11b6d3dac30d07e1aaba4a6a
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-26 23:56:10 -0400

    Added accepted python code for Valid Parenthesis problem

--- Leetcode Problems and Solutions/Easy_Difficulty/Valid_Parenthesis/20_Valid_Parenthesis.py
+++ Leetcode Problems and Solutions/Easy_Difficulty/Valid_Parenthesis/20_Valid_Parenthesis.py
@@ -10,9 +10,40 @@
 
 from typing import List, Tuple, Dict
 
-def isValid(self, s: str) -> bool:
+def isValid(s: str) -> bool:
 
-    pass    
+    parenthesis_dict = {
+        "[": "]",
+        "{": "}",
+        "(": ")"
+    }
+
+    # Implementing the stack as a python list
+    stack = []
+
+    # Loop through the input string
+    for char in s:
+        # Check to see if you have the opening bracket
+        if char in parenthesis_dict:
+            # If it is the opening bracket we push to the stack
+            stack.append(char)
+        
+        # If the character is a closing bracket
+        elif char in parenthesis_dict.values():
+
+            # Two conditions to return false:
+
+            # Either the stack is empty at this point, which means that we only have a closing bracket and not an opening bracket
+
+            # If the closing bracket we are at doesnt match the opening bracket, since every closing bracket can only come after the same opening bracket for it to be valid. So the second condition is to check if the closing bracket and the previous opening bracket are of the same type.
+
+            # parenthesis_dict[stack.pop()] gives the closing bracket that is expected for a valid combination, and we compare it with char which is the actual closing bracket we have in our input string
+
+            # And we only have opening brackets in our stack, so we get it through pop operation. This step also removes the element from the stack
+            if len(stack) == 0 or parenthesis_dict[stack.pop()] != char:
+                return False
+
+    return len(stack) == 0
 
 # Testing
 input_string1 = "()"
@@ -22,4 +53,4 @@ input_string4 = "([)]"
 input_string5 = "{[]}"
 
 
-print(isValid(input_string2))
+print(isValid(input_string1))
