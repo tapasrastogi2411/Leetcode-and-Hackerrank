cdf6a2e6a3212d62fbbc53a3e77b7fc60001c3fa
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-26 21:37:43 -0400

    Added python starter code for Valid Parenthesis problem

---
+++ Leetcode Problems and Solutions/Easy_Difficulty/Valid_Parenthesis/20_Valid_Parenthesis.py
@@ -0,0 +1,25 @@
+# Link to Problem: https://leetcode.com/problems/valid-parentheses/
+# Difficulty - Easy
+
+# Leetcode Problem number 20: Valid Parenthesis - Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+
+from typing import List, Tuple, Dict
+
+def isValid(self, s: str) -> bool:
+
+    pass    
+
+# Testing
+input_string1 = "()"
+input_string2 = "()[]{}"
+input_string3 = "(]"
+input_string4 = "([)]"
+input_string5 = "{[]}"
+
+
+print(isValid(input_string2))
