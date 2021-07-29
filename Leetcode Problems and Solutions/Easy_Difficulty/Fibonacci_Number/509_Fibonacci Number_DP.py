7e4599350c76d900837b4a83ff37dcef4c6a235c
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-29 02:30:21 -0400

    Added starter code for DP Python solution for Fibonacci Numbers

---
+++ Leetcode Problems and Solutions/Easy_Difficulty/Fibonacci_Number/509_Fibonacci Number_DP.py
@@ -0,0 +1,21 @@
+# Link to Problem: https://leetcode.com/problems/fibonacci-number/
+# Difficulty - Easy
+
+# Leetcode Problem number 509: Fibonacci Number: The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+# F(0) = 0, F(1) = 1
+# F(n) = F(n - 1) + F(n - 2), for n > 1.
+# Given n, calculate F(n).
+
+
+from typing import List, Tuple, Dict
+
+# Attempting a dynammic programming approach through memoization
+def fib_DP(n: int) -> int:
+    pass    
+    
+# Testing area
+
+input_number =  4
+
+print(fib(input_number))
