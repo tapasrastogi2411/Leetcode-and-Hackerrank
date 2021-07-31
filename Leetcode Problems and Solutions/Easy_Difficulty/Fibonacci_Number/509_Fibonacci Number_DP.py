9d9906b1a83f33288731c0a68c4ca37247b6ea2d
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-30 22:48:13 -0400

    Added accepted python code for Fibonacci number through DP

--- Leetcode Problems and Solutions/Easy_Difficulty/Fibonacci_Number/509_Fibonacci Number_DP.py
+++ Leetcode Problems and Solutions/Easy_Difficulty/Fibonacci_Number/509_Fibonacci Number_DP.py
@@ -12,10 +12,28 @@ from typing import List, Tuple, Dict
 
 # Attempting a dynammic programming approach through memoization
 def fib_DP(n: int) -> int:
-    pass    
+        
+     # The format of a memo: key: nth fib number, value: the value of the nth fib number
+    memo = {}
+        
+    # Checking if the number we are at is already computed and stored in the memo or not. And if it is, we just directly return it 
+    if n in memo:
+        return memo[n] # memo[n] stores the value of the nth fibonacci number
     
+    # Otherwise we continue with normal recursive fibonacci
+    if n < 2:
+        result = n
+        
+    else: 
+        result = fib_DP(n - 2) + fib_DP(n - 1)
+        
+    # Now we store the result that we have
+    memo[n] = result
+        
+    return result
+
 # Testing area
 
-input_number =  4
+input_number =  3
 
-print(fib(input_number))
+print(fib_DP(input_number))
