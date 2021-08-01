d922877f21357784b841a1b03b61458a9f77f3fc
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-07-31 23:43:36 -0400

    Added naive soution to house robber which doesnt work

--- Leetcode Problems and Solutions/Medium_Difficulty/House_Robber/198_House_Robber.py
+++ Leetcode Problems and Solutions/Medium_Difficulty/House_Robber/198_House_Robber.py
@@ -5,11 +5,25 @@
 
 # Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 
-def rob(nums: List[int]) -> int:
-    pass
+from typing import List, Tuple, Dict
 
+def rob(nums: List[int]) -> int:
+    
+    sum1 = 0
+    sum2 = 0
+    for i in range(0, len(nums), 2):
+        sum1 += nums[i]
+        
+    for j in range(1, len(nums), 2):
+        sum2 += nums[j]
+    
+    maximum_sum = max(sum1, sum2)
+    if maximum_sum == sum1 and maximum_sum == sum2:
+        # Then picking only adjacent houses most likely didnt work
+        pass
+    
 # Testing
 
-input_list = [1, 2, 3, 1]
+input_list = [2,1,1,2]
 
 print(rob(input_list))
