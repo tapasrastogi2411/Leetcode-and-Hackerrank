99c11843e40a1986f13f3c20dbdf5cfc83221373
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-01 23:55:36 -0400

    Added python starter code for the Generate Parentheses problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Generate_Parenthesis/22_Generate_Parenthesis.py
@@ -0,0 +1,14 @@
+# Link to Problem: https://leetcode.com/problems/generate-parentheses/
+# Difficulty - Medium
+
+# Leetcode Problem number 22: Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+from typing import List, Tuple, Dict
+
+def generateParentheses(n: int) -> List[str]:
+    print(3)
+
+# Testing
+input_number = 3
+
+print(generateParentheses(input_number))
