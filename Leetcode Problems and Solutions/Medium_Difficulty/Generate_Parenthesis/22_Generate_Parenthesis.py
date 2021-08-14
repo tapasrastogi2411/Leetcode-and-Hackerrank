0d080010385e4034f9cc7043211fe92c9bdd7447
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-13 23:41:19 -0400

    Added pseudocode and basic explanation on how to go about Generate Parenthesis

--- Leetcode Problems and Solutions/Medium_Difficulty/Generate_Parenthesis/22_Generate_Parenthesis.py
+++ Leetcode Problems and Solutions/Medium_Difficulty/Generate_Parenthesis/22_Generate_Parenthesis.py
@@ -5,8 +5,27 @@
 
 from typing import List, Tuple, Dict
 
+# Define a recursive helper function which we will use in the main `generateParentheses` function
+
+def backtracking(numOpen: int, numClose: int):
+    pass
+
+# The main function which will give the list of valid ordering of the parenthesis
 def generateParentheses(n: int) -> List[str]:
-    print(3)
+
+    # We can only add closing parenthesis when num(close) < num(open)
+
+    # We can add opening parenthesis at any point as long as num(open) < n
+
+    # Stopping condition, when we know that the ordering is valid: num(open) == num(close) == n - When we stop adding parenthesis
+
+    # A stack to hold parenthesis
+    stack = []
+    
+    # A list to hold the final ordering of parenthesis
+    result = []
+
+
 
 # Testing
 input_number = 3
