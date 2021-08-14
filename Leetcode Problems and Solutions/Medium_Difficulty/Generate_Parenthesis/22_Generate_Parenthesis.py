93344003ee4049a7068f62b8eef1bf107866c9a1
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-14 00:26:06 -0400

    Added accepted python solution for Generate_Parenthesis problem

--- Leetcode Problems and Solutions/Medium_Difficulty/Generate_Parenthesis/22_Generate_Parenthesis.py
+++ Leetcode Problems and Solutions/Medium_Difficulty/Generate_Parenthesis/22_Generate_Parenthesis.py
@@ -7,27 +7,50 @@ from typing import List, Tuple, Dict
 
 # Define a recursive helper function which we will use in the main `generateParentheses` function
 
-def backtracking(numOpen: int, numClose: int):
+def backtracking(resultList: List[str], parenthesis: str, numOpen: int, numClose: int, n: int):
+
+    # resultList is the list to which we add out valid orderings of parenthesis
+    # parenthesis is the valid ordering of parenthesis that we end up at each time
+    # numOpen is the number of open parenthesis that we have at a certain point
+    # numClose is the number of close parenthesis that we have at a certain point
+    # n is the pairs of parenthesis made available to us
+
+    if(len(parenthesis) == 2 *n):
+        # This means that we have a valid ordering and can append it to our result list
+        resultList.append(parenthesis)
+        return # This is our base case
+    
+    if numOpen < n:
+        # This means that we can add opening parenthesis to our current string of parenthesis
+        backtracking(resultList, parenthesis + "(", numOpen + 1, numClose, n)
+    
+    if numClose < numOpen:
+        # This means we can add a closing parenthesis to out current string of parenthesis
+        backtracking(resultList, parenthesis + ")", numOpen, numClose + 1, n)
+
+    
     pass
 
 # The main function which will give the list of valid ordering of the parenthesis
 def generateParentheses(n: int) -> List[str]:
+
+    # We are only allowed n open parenthesis and n closing parenthesis
 
     # We can only add closing parenthesis when num(close) < num(open)
 
     # We can add opening parenthesis at any point as long as num(open) < n
 
     # Stopping condition, when we know that the ordering is valid: num(open) == num(close) == n - When we stop adding parenthesis
-
-    # A stack to hold parenthesis
-    stack = []
     
     # A list to hold the final ordering of parenthesis
     result = []
 
+    backtracking(result, "", 0, 0, n) # Since we start with 0 open , 0 closed parenthesis and build from there
+    return result
+
 
 
 # Testing
-input_number = 3
+input_number = 100
 
 print(generateParentheses(input_number))
