3b188fdb033bd9c4664fda32d1df0920eb94d68c
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-16 10:28:31 -0400

    Added brute force Java solution to Longest Palindrome Problem

--- Leetcode Problems and Solutions/Medium_Difficulty/Longest_Palindromic_String/LongestPalindromicString.java
+++ Leetcode Problems and Solutions/Medium_Difficulty/Longest_Palindromic_String/LongestPalindromicString.java
@@ -1,35 +1,58 @@
 package Medium_Difficulty.Longest_Palindromic_String;
 
-import java.util.HashMap;
-
-/**
- * LongestPalindromicString
- */
 public class LongestPalindromicString {
 
-    public char longestPalindromicString(String s){
-        
-        int [] myArray = {10, 20, 30};
-        for(int i = 0; i < myArray.length; i++){
+    public boolean isPalindrome(String s){
 
-            System.out.println(myArray[i]);
+        for (int i = 0; i < s.length() / 2; i ++){
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)){
+                return false;
+            }
         }
-
-        HashMap<String, String> myHashMap = new HashMap<>();
-        myHashMap.put("Tapas", "Rastogi");
-        myHashMap.put("Roger", "Federer");
-
-        System.out.println(myHashMap);
-        System.out.println("The meaning of life is: " + myHashMap.containsKey("Tapas"));
-
-        return 'c';
+        return true;
 
     }
+        public String longestPalindrome(String s) {
+            
+            if (s.length() < 2){
+                return s;
+            }
+            
+            if(s.length() == 2){
+                if(isPalindrome(s)){
+                    return s;
+                }
+                return s.substring(0, 1);
+            }
+    
+            String result = "";
+            for(int i = 0; i < s.length(); i++){
+                for(int j = s.length() - 1; j > i; j--){
+    
+                    String substring = s.substring(i, j + 1);
+    
+                    if(isPalindrome(substring)){
+                        if(substring.length() > result.length()){
+                            result = substring;
+                        }
+                    }
+                }
+    
+            }
+            if(result.equals("")){
+                return s.substring(0, 1);
+            }
+            return result;
+      
+        }
 
     public static void main(String[] args) {
         
         LongestPalindromicString instance = new LongestPalindromicString();
-        instance.longestPalindromicString("Hello World");
+        String result = instance.longestPalindrome("cbbd");
+
+        System.out.println(result);
+
     }
 
     
