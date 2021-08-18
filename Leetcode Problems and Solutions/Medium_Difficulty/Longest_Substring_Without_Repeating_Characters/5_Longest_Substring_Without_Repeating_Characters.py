1799700141f7fd402a42e0b1ff29e72e17f9cc06
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-18 04:04:23 -0400

    Added python starter code for Longest Substring without Repeating charaters problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Longest_Substring_Without_Repeating_Characters/5_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,16 @@
+# Link to Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# Difficulty - Medium
+
+# Leetcode Problem number 5: Longest Substring without repeating characters: Given a string s, find the length of the longest substring without repeating characters.
+
+
+from typing import List, Tuple, Dict
+
+def lengthOfLongestSubstring(s: str) -> str:
+    return "234"
+
+# Testing
+
+input_string = "abcabcbb"
+
+print(lengthOfLongestSubstring(input_string))
