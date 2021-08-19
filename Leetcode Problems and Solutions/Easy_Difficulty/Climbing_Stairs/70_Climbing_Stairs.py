a2181d3951c6855a5ed9c34e36b033c4a1a4bc45
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-18 23:51:40 -0400

    Added python starter code for climbing stairs problem

---
+++ Leetcode Problems and Solutions/Easy_Difficulty/Climbing_Stairs/70_Climbing_Stairs.py
@@ -0,0 +1,15 @@
+# Link to problem: https://leetcode.com/problems/climbing-stairs/
+# Difficulty level: Easy
+
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+def climbStairs(n: int) -> int:
+    pass
+
+# Testing
+
+n = 5
+
+print(climbingStairs(n))
