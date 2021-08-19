15becc9fa0ee956962619c55a2a42f83802ea417
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-19 09:17:15 -0400

    Added python starter code for Unique paths problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Unique_Paths/62_Unique_Paths.py
@@ -0,0 +1,19 @@
+'''
+Link to problem: https://leetcode.com/problems/unique-paths/
+Difficulty level: Medium
+
+Leetcode problem number 62: Unique paths: A robot is located at the top-left corner of a m x n grid
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+'''
+
+def uniquePaths(m: int, n: int) -> int:
+    pass
+
+
+
+# Testing
+
+print(uniquePaths(3, 7))
