b3973554e74509261dd59a502454189a11ac0a52
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-19 09:27:58 -0400

    Added python starter code for Letter combinations of a phone number problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Letter_Combinations_Of_A_Phone_Number/17_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,19 @@
+'''
+Link to problem: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+Difficulty level: Medium
+
+Leetcode problem number 17: Letter combinations of a phone number: Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+'''
+
+from typing import List, Tuple, Dict
+
+def letterCombinations(digits: str) -> List[str]:
+    pass
+
+# Testing 
+
+input_digits = "23"
+
+print(letterCombinations(input_digits))
