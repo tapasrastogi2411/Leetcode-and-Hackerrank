0d30c8ffc38719006ac2d7f96a599b8dbad2f251
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-20 01:21:35 -0400

    Added python starter code for Rotate Image problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Rotate_image/48_Rotate_Image.py
@@ -0,0 +1,23 @@
+'''
+Link to problem: https://leetcode.com/problems/rotate-image/
+Difficulty level: Medium
+
+Leetcode problem number 48: Rotate image: You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation
+'''
+
+from typing import List, Tuple, Dict
+
+def rotate(matrix: List[List[int]]) -> None:
+
+    pass
+
+# Testing
+
+input_matrix = [
+
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
