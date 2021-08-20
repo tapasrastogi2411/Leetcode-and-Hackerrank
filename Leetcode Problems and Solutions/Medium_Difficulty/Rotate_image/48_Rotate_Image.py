55dffee1337e623b0f80e7f54e68be13831aa2eb
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-20 01:27:03 -0400

    Added a 32 ms accepted python solution

--- Leetcode Problems and Solutions/Medium_Difficulty/Rotate_image/48_Rotate_Image.py
+++ Leetcode Problems and Solutions/Medium_Difficulty/Rotate_image/48_Rotate_Image.py
@@ -9,9 +9,38 @@ You have to rotate the image in-place, which means you have to modify the input 
 
 from typing import List, Tuple, Dict
 
+# A 32 ms accepted solution with detailed explanation
 def rotate(matrix: List[List[int]]) -> None:
 
-    pass
+    length = len(matrix)
+        
+    '''
+    Rotating 90 degrees clockwise means that we have to transpose first and then 
+    reflecting
+    '''      
+        
+    for i in range(length):
+        for j in range(i):
+                
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+        
+    '''Now we reflect the matrix from the right to the left. This is similar to iterative
+    version of checking if a string was a palindrome where we loop till n // 2 and 
+    check the first element with the last element, the second element with the second 
+    last element and so on, till the middle element
+        
+    Reflecting here basically means switching the first element with the last element
+    and so on
+    '''
+        
+    for i in range(length):
+        for j in range(length // 2):
+                
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][length - j - 1] # similar to [n - i - 1]
+            matrix[i][length - j - 1] = temp
 
 # Testing
 
@@ -21,3 +50,7 @@ input_matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
+
+rotate(input_matrix)
+
+print(f"Rotate matrix is: {input_matrix}")
