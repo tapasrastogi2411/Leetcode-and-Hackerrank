b87bfdc27a4bbd98474760a82d8b0957b863105b
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-25 19:18:31 -0400

    Added python starter code for Maximum Subarray problem

---
+++ Leetcode Problems and Solutions/Easy_Difficulty/Maximum_SubArray/53_Maximum_Subarray.py
@@ -0,0 +1,19 @@
+'''
+Link to problem: https://leetcode.com/problems/maximum-subarray/
+Difficulty level: Easy
+
+Leetcode problem number 53: Maximum Subarray: Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+'''
+
+from typing import List, Tuple, Dict
+
+def maxSubArray(nums: List[int]) -> int:
+    pass
+
+# Testing
+
+input_nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+print(maxSubArray(input_nums))
