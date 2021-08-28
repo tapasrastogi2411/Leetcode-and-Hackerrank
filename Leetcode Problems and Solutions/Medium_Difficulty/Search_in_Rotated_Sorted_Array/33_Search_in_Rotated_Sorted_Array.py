aaa7b16267db8fe2e3d848af797abed844a1383b
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-27 21:02:12 -0400

    Added python starter code for Search in Rotated Sorted Array Problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Search_in_Rotated_Sorted_Array/33_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,25 @@
+'''
+Link to problem: https://leetcode.com/problems/search-in-rotated-sorted-array/
+Difficulty level: Medium
+
+Leetcode problem number 33: Search in Rotated Sorted Array: There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+'''
+
+from typing import List, Tuple, Dict
+
+def search(nums: List[int], target: int) -> int:
+    pass
+
+# Testing
+input_nums = [4,5,6,7,0,1,2]
+input_target = 0
+
+print(search(input_nums, input_target))
