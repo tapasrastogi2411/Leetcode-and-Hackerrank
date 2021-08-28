c32c4b3fd4c69e451c0f8fa7a9ca9cc762c233c4
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-08-28 18:39:23 -0400

    Added comments explaining my solution and algorithm for Letter Combinations problem

--- Leetcode Problems and Solutions/Medium_Difficulty/Letter_Combinations_Of_A_Phone_Number/17_Letter_Combinations_of_a_Phone_Number.py
+++ Leetcode Problems and Solutions/Medium_Difficulty/Letter_Combinations_Of_A_Phone_Number/17_Letter_Combinations_of_a_Phone_Number.py
@@ -11,14 +11,19 @@ from typing import List, Tuple, Dict
 
 def generateCombinations(currentString: str, digitToChar: Dict[str, str], digits: str, index: int, result: List[str]) -> None:
     
+    # Our Base case: If the string we are is same length as the input digits, it means we have one possible combination and we can add it our final result list
     if len(currentString) == len(digits):
         result.append(currentString)
 
     else:
 
+        # Get the current digit that we are at in the input string
         currentDigit = digits[index]
+
+        # Get the letters that the current digit maps to
         mappingForDigit = digitToChar[currentDigit]
 
+        # We add each character to the current string, and increment index by 1 to go to the next digit in the input string, since each digit only contributes one digit
         for char in mappingForDigit:
             generateCombinations(currentString + char, digitToChar, digits, index + 1, result)
 
@@ -38,10 +43,13 @@ def letterCombinations(digits: str) -> List[str]:
         
     }
 
+    # A variable to store the final list of possible letter combinations
     result = []
 
+    # If the input is not empty, then we pen up a recursive backtracking solution
     if len(digits) != 0:
 
+        # This is the recursive helper function which does most of the work and modifies the result list in place returning nothing
         generateCombinations("", digitToChar, digits, 0, result)
 
 
