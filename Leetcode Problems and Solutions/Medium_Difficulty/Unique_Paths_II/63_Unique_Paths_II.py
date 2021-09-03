195b92df474b7ac3cd0a2b5942520e5f612717fc
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-09-03 18:29:16 -0400

    Added comments explaining the accepted solution to Unique Paths II

--- Leetcode Problems and Solutions/Medium_Difficulty/Unique_Paths_II/63_Unique_Paths_II.py
+++ Leetcode Problems and Solutions/Medium_Difficulty/Unique_Paths_II/63_Unique_Paths_II.py
@@ -32,10 +32,12 @@ def uniquePathsWithObstacles(obstacleGrid: List[List[int]]) -> int:
         
     # Calculating the number of ways to reach cell in the first row
     for i in range(1, numColumns):
-            
+
+        # If the cell we are at is value 1, then it means that it is an obstacle and thereis no way we can ever reach it, and hence the number of ways to reach that cell is 0   
         if obstacleGrid[0][i] == 1:
             dp_array[0][i] = 0
-            
+        
+        # Else the only way to reach the cell in the first row is from the cell before it, since if the previous cell is an obstacle then, the number of ways to reach any of the cells after that will always be 0
         else:
             dp_array[0][i] = dp_array[0][i - 1]
             
@@ -44,21 +46,28 @@ def uniquePathsWithObstacles(obstacleGrid: List[List[int]]) -> int:
         if obstacleGrid[i][0] == 1:
                 
             dp_array[i][0] = 0
-           
+        
+          
+        # The number of ways to reach any cell in the first column, depend on the number of ways we can we can reach the cell above it, this is because of the obstacles we have in place
         else:
             dp_array[i][0] = dp_array[i - 1][0]
         
-        
+    # Calculating the number of ways to reach all the other cells in the grid
     for i in range(1, numRows):
         for j in range(1, numColumns):
 
+            '''This is similar to unique paths 1 problem, where the number of ways to reach any
+            inner cell is the sum of the number of ways we can reach the cell above it and 
+            the number of ways we can reach the cell below it '''
             if obstacleGrid[i][j] == 0:
             
                 dp_array[i][j] = dp_array[i - 1][j] + dp_array[i][j - 1]
-            
+
+            # We have an obstacle and then number of ways we can reach a cell with an obstacle is 0
             else:
                 dp_array[i][j] = 0
-        
+                
+    # The number of ways to reach the finish is the value held in the final cell of the dp_array
     return dp_array[numRows - 1][numColumns - 1]
 
 # Testing
