050854af66b331f04a059549cebfadf00f73380e
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-09-03 16:06:42 -0400

    Added python starter code for the Unique Path II problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Unique_Paths_II/63_Unique_Paths_II.py
@@ -0,0 +1,29 @@
+'''
+Link to problem: https://leetcode.com/problems/unique-paths-ii/
+Difficulty level: Medium
+
+Leetcode problem number 63: Unique paths: A robot is located at the top-left corner of a m x n grid
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and space is marked as 1 and 0 respectively in the grid.
+'''
+from typing import List
+
+def uniquePathsWithObstacles(obstacleGrid: List[List[int]]) -> int:
+
+    pass
+
+# Testing
+
+input_grid = [
+
+    [0,0,0],
+    [0,1,0],
+    [0,0,0]
+
+    ]
+
+print(uniquePathsWithObstacles(input_grid))
