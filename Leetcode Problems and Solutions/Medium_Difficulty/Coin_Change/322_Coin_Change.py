e51ad34e4fb130aacffa53805226460e3334a710
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-09-04 22:56:29 -0400

    Added python starter code for Coin Change Problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Coin_Change/322_Coin_Change.py
@@ -0,0 +1,22 @@
+'''
+Link to problem: https://leetcode.com/problems/coin-change/
+Difficulty level: Medium
+
+Leetcode problem number 322: Coin Change: You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+'''
+
+from typing import List
+
+def coinChange(coins: List[int], amount: int) -> int:
+    pass
+
+# Testing 
+
+input_coins = [1, 2, 5]
+input_amount = 11
+
+print(coinChange(input_coins, input_amount))
