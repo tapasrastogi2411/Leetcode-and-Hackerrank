d2cacc81cbaf6d6c22d93bd0922819a8e65eca30
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-09-07 18:12:55 -0400

    Created first linked list to test code

--- Leetcode Problems and Solutions/Easy_Difficulty/Merge_Two_Sorted_Lists/21_Merge_Two_Sorted_Lists.c
+++ Leetcode Problems and Solutions/Easy_Difficulty/Merge_Two_Sorted_Lists/21_Merge_Two_Sorted_Lists.c
@@ -20,6 +20,8 @@ struct ListNode *initialiser(int nodeValue){
 
     newNode -> val = nodeValue;
     newNode -> next = NULL;
+
+    return newNode;
 }
 
 struct ListNode *printList(struct ListNode *head){
@@ -29,19 +31,47 @@ struct ListNode *printList(struct ListNode *head){
     while(temp != NULL){
 
         printf("%d ", temp -> val);
-
         temp = temp -> next;
     }
+
     printf("\n");
 }
 
 struct ListNode *addAtHead(struct ListNode *head, struct ListNode *nodeToAdd){
 
     // Making the next pointer of nodeToAdd point to head
-
     nodeToAdd -> next = head;
 
     // nodeToAdd is our new head, so we return that
-
     return nodeToAdd;
 }
+
+// The main method that we have to implement
+struct ListNode *mergeTwoLists(struct ListNode *l1, struct ListNode *l2){
+
+    // TODO: Implement this correctly
+    return l1;
+}
+
+int main(){
+
+    // Writing driver code to run and test our method
+
+    // Creating the first linked list
+    struct ListNode *head1 = NULL;
+
+    struct ListNode *head1Node1 = initialiser(1);
+    struct ListNode *head1Node2 = initialiser(2);
+    struct ListNode *head1Node3 = initialiser(4);
+
+    // Conecting these three nodes together
+    head1 = addAtHead(head1, head1Node3);
+    head1 = addAtHead(head1, head1Node2);
+    head1 = addAtHead(head1, head1Node1);
+
+    printList(head1);
+    
+    
+}
+
+
