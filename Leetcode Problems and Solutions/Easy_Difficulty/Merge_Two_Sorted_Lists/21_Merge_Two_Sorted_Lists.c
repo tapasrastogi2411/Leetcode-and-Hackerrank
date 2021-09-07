d3e5f3fed2efa3b85a9fb9ff25fc22257ed02f60
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-09-07 17:56:35 -0400

    Added problem description and definition of LL Node

---
+++ Leetcode Problems and Solutions/Easy_Difficulty/Merge_Two_Sorted_Lists/21_Merge_Two_Sorted_Lists.c
@@ -0,0 +1,12 @@
+/*
+Link to problem: https://leetcode.com/problems/merge-two-sorted-lists/
+Difficulty level: Easy
+
+Leetcode problem number 21: Merge Two Sorted Lists: Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists
+
+*/
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
