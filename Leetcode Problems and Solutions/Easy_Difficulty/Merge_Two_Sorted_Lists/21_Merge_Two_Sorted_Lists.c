af0b7e145d9ccb0348a57278107749ebb399e7da
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-09-08 23:34:29 -0400

    Added comments explaining C solution to problem

--- Leetcode Problems and Solutions/Easy_Difficulty/Merge_Two_Sorted_Lists/21_Merge_Two_Sorted_Lists.c
+++ Leetcode Problems and Solutions/Easy_Difficulty/Merge_Two_Sorted_Lists/21_Merge_Two_Sorted_Lists.c
@@ -49,44 +49,61 @@ struct ListNode *addAtHead(struct ListNode *head, struct ListNode *nodeToAdd){
 // The main method that we have to implement
 struct ListNode *mergeTwoLists(struct ListNode *l1, struct ListNode *l2){
 
+    // Initialising the head of the new merged linked list
     struct ListNode *mergedHead = initialiser(0);
+
+    // Using a dummy temporary listnode for building the merged list
     struct ListNode *dummy = mergedHead;
     
+    // We loop through the l1, l2 and merged lists till either one the l1 or l2 lists becomes empty
     while(l1 != NULL && l2 != NULL){
         
+        // If the value we are is smaller at l1 than l2, we create a node with the l1_val and add to the merged list
         if(l1 -> val < l2 -> val){
             
             // The value that we will add to mergedHead is the value at l1
             struct ListNode *newMergedNode = initialiser(l1 -> val);
+
+            // Connecting this newly created node to our merged list
             dummy -> next = newMergedNode;
             
+            // Moving along in the l1 list. A condition necessary for termination
             l1 = l1 -> next;
         
         }
         
-        
+        // The second case: When the value at l2 is smaller than the corresponding value in l1
         else {
             
+            // Create a new node with l2_val to be added to our merged list
             struct ListNode *newMergedNode = initialiser(l2 -> val);
+
+            // Add this node to the merged list
             dummy -> next = newMergedNode;
             
+            // Move along to the next node in the l2 list
             l2 = l2 -> next;
         }
         
+        // Move along in the merged list after one node has been added
         dummy = dummy -> next;
             
         }
     
-    
+    // There are two ways for the above while loop to terminate. Either l1 is empty or l2 is empty
+
+    // If l1 is empty, then we add the remaining of the l2 list to our merged list
     if(l1 == NULL){
         
         dummy -> next = l2;
     }
     
+    // If l2 is empty, we add the remaining of the l1 list to the end of our merged list
     if(l2 == NULL){
         dummy -> next = l1;
     }
 
+    // Recall how we created dummy ListNode only to reference the whole merged linked list at the end. mergedHead -> next is what starts the beginning of the dummy list, which actually is our final merged list
     return mergedHead -> next;
     
 }
