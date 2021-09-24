21c6ad433dcfdf694eac1c19969b331983cec9d2
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-09-23 23:42:57 -0400

    Added Maximum Product Subarray problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Max_Product_SubArray/152_Max_Product_Subarray.py
@@ -0,0 +1,23 @@
+'''
+Link to problem: https://leetcode.com/problems/maximum-product-subarray/
+Difficulty level: Medium
+
+Leetcode problem number 152: Max Product Subarray: Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+
+It is guaranteed that the answer will fit in a 32-bit integer.
+
+A subarray is a contiguous subsequence of the array.
+ '''
+
+from typing import List
+
+def maxProduct(self, nums: List[int]) -> int:
+
+    pass
+
+# Testing
+input_nums1 = [2,3,-2,4]
+
+input_nums2 = [-2, 0, -1]
+
+print()
