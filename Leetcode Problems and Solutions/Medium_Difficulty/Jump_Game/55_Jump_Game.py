8bbfb9daaf5a108be908f5bf3bfca56ba73a6e8c
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2022-01-31 00:25:54 -0500

    Added starter code for the Jump Game problem

---
+++ Leetcode Problems and Solutions/Medium_Difficulty/Jump_Game/55_Jump_Game.py
@@ -0,0 +1,19 @@
+'''
+Link to problem: https://leetcode.com/problems/jump-game/
+Difficulty level: Medium
+
+Leetcode problem number 5: Jump Game: You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+'''
+
+from typing import List, Dict, Tuple
+
+def canJump(nums: List[int]) -> bool:
+
+    pass
+
+# Testing
+input_nums = [2, 3, 1, 1, 4]
+
+print(canJump(input_nums))
